839e7dccdc559c4219d289ef5413a15134a27d71
Austin Lingenfelter <austin.lingenfelter@gmail.com>
2025-06-05 16:03:02 -0400

    troubleshooting kairos image builder

--- kairos-image/aurora-boot.sh
+++ kairos-image/aurora-boot.sh
@@ -3,12 +3,12 @@ IMAGE=quay.io/kairos/ubuntu:24.04-standard-arm64-generic-v3.4.2-k3sv1.32.3-k3s1
 docker pull $IMAGE
 
 docker run -v $PWD/config.yaml:/config.yaml \
-           -v $PWD/build:/tmp \
+           -v $PWD/build:/tmp/auroraboot \
            -v /var/run/docker.sock:/var/run/docker.sock \
-           --rm -ti kairos.docker.scarf.sh/kairos/auroraboot:v0.6.4 \
+           --rm -ti kairos.docker.scarf.sh/kairos/auroraboot \
            --set "disable_http_server=true" \
            --set "disable_netboot=true" \
            --set "container_image=docker://$IMAGE" \
-           --set "iso.data=/tmp/data" \
+           --set repository=kairos-io/kairos \
            --cloud-config /config.yaml \
            --set "state_dir=/tmp/auroraboot"
